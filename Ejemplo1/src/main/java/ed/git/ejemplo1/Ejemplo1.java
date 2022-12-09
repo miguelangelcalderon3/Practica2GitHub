d7e54abb228eaa084f6ab967669b85713ef24524
miguelangelcalderon3 <Miguelangel.calderon@iesmiguelherrero.com>
2022-12-09 19:38:33 +0100

    Tarea2 1.0

---
+++ Ejemplo1/src/main/java/ed/git/ejemplo1/Ejemplo1.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package ed.git.ejemplo1;
+
+/**
+ *
+ * @author macz
+ */
+public class Ejemplo1 {
+
+    public static void main(String[] args) {
+        System.out.println("Primer ejemplo para el main");
+    }
+}
